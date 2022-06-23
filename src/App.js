1128bffd9e5defbee1d0ff3099a8739e9bb217c7
dev_nayan <md.nayan8494@gmail.com>
2022-06-24 01:30:13 +0600

    Navbar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Homepage from "./Pages/Homepage/Homepage";
 import NotFound from "./Pages/NotFound/NotFound";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
         <Route path="*" element={<NotFound />}></Route>
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Logo from '../../../assets/logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="warning" variant="light">
+      <Container>
+        <Link to={'/'}><img src={Logo} className='w-75' alt="ToolFacturer" /></Link>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Link className='nav-link text-dark' to={'/blogs'}>Blogs</Link>
+            <Link className='nav-link text-dark' to={'/portfolio'}>Portfolio</Link>
+          </Nav>
+          <Nav>
+          <Link className='nav-link text-dark' to={'/login'}>Login</Link>
+          <Link className='nav-link text-dark' to={'/register'}>Register</Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+};
+
+export default Header;
